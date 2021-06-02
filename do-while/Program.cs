f704f4b99cf833fd066b7370e17bd4c6fd115331
Soumay kanti Das <anikds2859@outlook.com>
2021-06-02 13:03:41 +0600

    Add project files.

---
+++ do-while/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+
+            } while (x <= 5);
+        }
+    }
+}
